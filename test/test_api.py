230361f1b2355ace244929b37c74154da74bd0c3
sumeyaaaa <sumeyasirmulach@gmail.com>
2025-07-01 18:39:39 +0300

    Add FastAPI app with Docker, CI/CD, and linter checking flake8 and pytest

--- test/test_api.py
+++ test/test_api.py
@@ -1,10 +1,11 @@
-from unittest.mock import MagicMock, patch
 import sys
 import os
 import warnings
+from unittest.mock import MagicMock, patch
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+# Add src folder to sys.path so 'api' can be imported
 sys.path.insert(
     0,
     os.path.abspath(
@@ -19,43 +20,48 @@ sys.path.insert(
 mock_model = MagicMock()
 mock_model.predict_proba.return_value = [[0.3, 0.7]]
 
-with patch('mlflow.sklearn.load_model', return_value=mock_model):
-    from fastapi.testclient import TestClient
-    from api.main import app
+patcher = patch('mlflow.sklearn.load_model', return_value=mock_model)
+patcher.start()
 
-    client = TestClient(app)
+from fastapi.testclient import TestClient  # noqa: E402
+from api.main import app   # noqa: E402
+client = TestClient(app)
 
-    def test_predict():
-        sample_data = {
-            "Recency": 1,
-            "Frequency": 127,
-            "Monetary": 489358,
-            "Transaction_Hour": 8,
-            "FraudResult": 0,
-            "Average_Transaction_Amount": 2424.58,
-            "Transaction_Day": 12,
-            "ChannelId_ChannelId_2": False,
-            "ChannelId_ChannelId_3": True,
-            "ChannelId_ChannelId_5": False,
-            "ProviderId_ProviderId_2": False,
-            "ProviderId_ProviderId_3": False,
-            "ProviderId_ProviderId_4": False,
-            "ProviderId_ProviderId_5": False,
-            "ProviderId_ProviderId_6": True,
-            "PricingStrategy_1": False,
-            "PricingStrategy_2": True,
-            "PricingStrategy_4": False,
-            "ProductCategory_data_bundles": False,
-            "ProductCategory_financial_services": False,
-            "ProductCategory_movies": False,
-            "ProductCategory_other": False,
-            "ProductCategory_ticket": False,
-            "ProductCategory_transport": False,
-            "ProductCategory_tv": False,
-            "ProductCategory_utility_bill": False,
-        }
 
-        response = client.post("/predict", json=sample_data)
-        assert response.status_code == 200
-        assert "risk_probability" in response.json()
-        assert abs(response.json()["risk_probability"] - 0.7) < 1e-6
+def test_predict():
+    sample_data = {
+        "Recency": 1,
+        "Frequency": 127,
+        "Monetary": 489358,
+        "Transaction_Hour": 8,
+        "FraudResult": 0,
+        "Average_Transaction_Amount": 2424.58,
+        "Transaction_Day": 12,
+        "ChannelId_ChannelId_2": False,
+        "ChannelId_ChannelId_3": True,
+        "ChannelId_ChannelId_5": False,
+        "ProviderId_ProviderId_2": False,
+        "ProviderId_ProviderId_3": False,
+        "ProviderId_ProviderId_4": False,
+        "ProviderId_ProviderId_5": False,
+        "ProviderId_ProviderId_6": True,
+        "PricingStrategy_1": False,
+        "PricingStrategy_2": True,
+        "PricingStrategy_4": False,
+        "ProductCategory_data_bundles": False,
+        "ProductCategory_financial_services": False,
+        "ProductCategory_movies": False,
+        "ProductCategory_other": False,
+        "ProductCategory_ticket": False,
+        "ProductCategory_transport": False,
+        "ProductCategory_tv": False,
+        "ProductCategory_utility_bill": False,
+    }
+
+    response = client.post("/predict", json=sample_data)
+    assert response.status_code == 200
+    assert "risk_probability" in response.json()
+    assert abs(response.json()["risk_probability"] - 0.7) < 1e-6
+
+
+patcher.stop()
